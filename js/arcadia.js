c1074a0c3bb8bcbac0f000e29c451f91f84d82ae
YangHyunchul <wolfses@hotmail.com>
2013-05-30 13:03:36 +0900

    Personal Dashboard - ver.20130529 #1
    1. 화면 크기 확대시 깨지는 문제 해결
    2. ANSI로 되어있던 파일 UTF-8 변환
    3. popup information 구현

--- js/arcadia.js
+++ js/arcadia.js
@@ -1,3 +1,10 @@
+var popupmenu_info = [
+	{title: "Fat", content: "코드에서 객체간 연관 정도를 보여주는 지표이다."},
+	{title: "Coupling", content: "커플링 관련 지표이다."},
+	{title: "Naming", content: "변수나 객체의 작명 규칙 위반의 정도를 보여주는 지표이다."},
+	{title: "Basic Rule", content: "PMD를 통해 나온 정보로 규칙 위반의 정도를 보여주는 지표이다."}
+];
+
 function supportsSVG() {
 	return !!document.createElementNS && !!document.createElementNS('http://www.w3.org/2000/svg', "svg").createSVGRect && !!(window.SVGSVGElement);
 }
@@ -5,7 +12,6 @@ function supportsSVG() {
 function resizeEvent(obj)
 {
 	$("#content").css("min-height", "0px");
-	$("#content").css("width", $(window).width() - 143);
 	if($("#content").height() > $(window).height() - 74)
 	{
 		$("#sidebar-container").css("min-height", $("#content").height() );
@@ -41,4 +47,44 @@ $(document).ready(function() {
 			navigator.userAgent.match(/windows/i) ) $("#container").addClass("noinset");
 	}
 	window.onresize = resizeEvent;
+
+	popup_hide = function() {
+		$("#popup-info").stop(true, true);
+		$("#popup-info").animate({
+			opacity: 0.0,
+		}, 250, function(){
+			$(this).css("display", "none");
+		});
+	};
+	popup_show = function(obj) {
+		$("#popup-info").stop(true, true);
+		$("#popup-info").css("display", "block");
+		$("#popup-info").animate({ opacity: 0.8 }, 250, function() {} );
+		$("#popup-info").css("top", obj.position().top + 45);
+		$("#popup-info").css("left", obj.position().left - 100);
+	};
+
+	$("#project-list .list-item .fat").hover(function() {
+		$("#popup-title").html(popupmenu_info[0].title);
+		$("#popup-content").html(popupmenu_info[0].content);
+		popup_show($(this));
+	},popup_hide);
+
+	$("#project-list .list-item .coupling").hover(function() {
+		$("#popup-title").html(popupmenu_info[1].title);
+		$("#popup-content").html(popupmenu_info[1].content);
+		popup_show($(this));
+	},popup_hide);
+
+	$("#project-list .list-item .naming").hover(function() {
+		$("#popup-title").html(popupmenu_info[2].title);
+		$("#popup-content").html(popupmenu_info[2].content);
+		popup_show($(this));
+	},popup_hide);
+
+	$("#project-list .list-item .basicrule").hover(function() {
+		$("#popup-title").html(popupmenu_info[3].title);
+		$("#popup-content").html(popupmenu_info[3].content);
+		popup_show($(this));
+	},popup_hide);
 });
